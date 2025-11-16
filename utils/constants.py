0953af42f16936104661c6812a43b46b07007874
anandrei90 <andrei90angelescu@gmail.com>
2025-11-16 17:17:16 +0100

    Refactor constants.py to reflect new folder structure

--- utils/constants.py
+++ utils/constants.py
@@ -1,11 +1,11 @@
 import sys
-from os.path import join
+from os.path import join, dirname
 
-WORKING_DIR = sys.path[0]
-TRAIN_PATH_BOARDS = join(WORKING_DIR, "data", "train")
-TEST_PATH_BOARDS = join(WORKING_DIR, "data", "test")
-TRAIN_PATH_PIECES = join(WORKING_DIR, "data", "pieces_train_no_duplicates")
-TEST_PATH_PIECES = join(WORKING_DIR, "data", "pieces_test")
-MODEL_PATH = join(WORKING_DIR, "model_checkpoints", "chess_pieces.keras")
+PROJECT_DIR = dirname(sys.path[0])
+TRAIN_PATH_BOARDS = join(PROJECT_DIR, "data", "train")
+TEST_PATH_BOARDS = join(PROJECT_DIR, "data", "test")
+TRAIN_PATH_PIECES = join(PROJECT_DIR, "data", "pieces_train_no_duplicates")
+TEST_PATH_PIECES = join(PROJECT_DIR, "data", "pieces_test")
+MODEL_PATH = join(PROJECT_DIR, "model_checkpoints", "chess_pieces.keras")
 BATCH_SIZE = 640
 CLASS_NAMES = ['e', 'P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k']
